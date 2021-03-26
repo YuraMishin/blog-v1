7234b3e0ab87be68b16edf1570693258d433db7b
Mishin Yura <mishin.inbox@gmail.com>
2021-03-26 08:46:15 +0300

    Added basic settings. Closes #3

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,26 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Web
 {
-    public class Program
+  /// <summary>
+  /// Class Program.
+  /// Implements the main app class
+  /// </summary>
+  public class Program
+  {
+    /// <summary>
+    /// App entry point
+    /// </summary>
+    /// <param name="args">args</param>
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      CreateHostBuilder(args).Build().Run();
     }
+
+    /// <summary>
+    /// Method bootstraps the app
+    /// </summary>
+    /// <param name="args">args</param>
+    /// <returns>IHostBuilder</returns>
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -1,40 +1,77 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Web
 {
-    public class Startup
+  /// <summary>
+  /// Class Startup.
+  /// Implements initial settings
+  /// </summary>
+  public class Startup
+  {
+    /// <summary>
+    /// Configuration
+    /// </summary>
+    private readonly IConfiguration _configuration;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private readonly IWebHostEnvironment _env;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="configuration">configuration</param>
+    /// <param name="env">env</param>
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
-        }
+      _configuration = configuration;
+      _env = env;
     }
+
+    /// <summary>
+    /// Method gets called by the runtime to add services to the container
+    /// </summary>
+    /// <param name="services">services</param>
+    public void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    /// <summary>
+    /// Method gets called by the runtime to configure the HTTP request pipeline
+    /// </summary>
+    /// <param name="app">app</param>
+    /// <param name="logger">logger</param>
+    public void Configure(IApplicationBuilder app, ILogger<Startup> logger)
+    {
+      logger.LogInformation("App is running. Enjoy!");
+
+      if (_env.IsDevelopment())
+      {
+        logger.LogInformation("Development mode");
+        app.UseDeveloperExceptionPage();
+      }
+
+      if (_env.IsProduction())
+      {
+        logger.LogInformation("Production mode");
+      }
+
+      app.UseRouting();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapGet("/", async context =>
+              {
+                await context.Response.WriteAsync("Hello World!");
+              });
+      });
+    }
+  }
 }
